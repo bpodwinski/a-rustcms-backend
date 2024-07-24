51d7ac094be63c81601d3e6d8a11ee032b83419f
Benoit Podwinski <me@benoitpodwinski.com>
2024-07-24 11:09:07 +0200

    Add CORS to env var

--- src/config/config.rs
+++ src/config/config.rs
@@ -3,3 +3,7 @@ use std::env;
 pub fn get_database_url() -> String {
     env::var("DATABASE_URL").expect("DATABASE_URL must be set")
 }
+
+pub fn get_cors_allowed_url() -> String {
+    env::var("CORS_ALLOWED_URL").expect("CORS_ALLOWED_URL must be set")
+}
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ mod routes;
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let pool = db::init_pool().await.expect("Failed to create pool");
+    let cors_allowed_url = crate::config::config::get_cors_allowed_url();
 
     HttpServer::new(move || App::new()
         .wrap(
             Cors::new()
-                .allowed_origin("http://127.0.0.1:3000")
+                .allowed_origin(&cors_allowed_url)
                 .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                 .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
                 .allowed_header(http::header::CONTENT_TYPE)
