09716243edb3a8af098244d9b636b7a3eafaff3e
Benoit Podwinski <me@benoitpodwinski.com>
2024-08-29 17:31:16 +0200

    Fix delete post controller

--- src/controllers/posts/delete_post.rs
+++ src/controllers/posts/delete_post.rs
@@ -1,13 +1,9 @@
 use ntex::web::{self, HttpResponse};
-use serde::Serialize;
 use sqlx::PgPool;
 
-use crate::services::posts::delete_post::delete_post_service;
-
-#[derive(Serialize)]
-struct ResponseMessage {
-    message: String,
-}
+use crate::{
+    dto::ErrorMessage, services::posts::delete_post::delete_post_service,
+};
 
 #[web::delete("/posts/{id}")]
 pub async fn delete_post_controller(
@@ -16,11 +12,11 @@ pub async fn delete_post_controller(
 ) -> HttpResponse {
     match delete_post_service(pool.get_ref(), post_id.into_inner()).await {
         Ok(rows_affected) if rows_affected > 0 => {
-            HttpResponse::Ok().json(&ResponseMessage {
-                message: "Post deleted successfully".to_string(),
-            })
+            HttpResponse::NoContent().finish()
         }
-        Ok(_) => HttpResponse::NotFound().body("Post not found"),
+        Ok(_) => HttpResponse::NotFound().json(&ErrorMessage {
+            error: "Post not found".to_string(),
+        }),
         Err(err) => {
             eprintln!("Failed to delete post: {:?}", err);
             HttpResponse::InternalServerError().finish()
--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -1,2 +1,9 @@
+use serde::Serialize;
+
 pub mod category_dto;
 pub mod post_dto;
+
+#[derive(Serialize)]
+pub struct ErrorMessage {
+    pub error: String,
+}
