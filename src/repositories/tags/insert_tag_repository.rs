7d1561b972e7d153ade9ba2098e55d4730d739ee
Benoit Podwinski <me@benoitpodwinski.com>
2024-09-07 22:23:55 +0200

    Add unit test for insert tag repository

--- src/repositories/tags/insert_tag_repository.rs
+++ src/repositories/tags/insert_tag_repository.rs
@@ -28,3 +28,35 @@ pub async fn insert(
 
     Ok(tag)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{db, models::tags::tags_table_model::TagModel};
+    use dotenv::dotenv;
+
+    #[tokio::test]
+    async fn test_insert_tag() {
+        //Arrange
+        dotenv().ok();
+        let pool = db::init_pool().await.expect("Failed to create pool");
+
+        let new_tag = TagModel {
+            id: None,
+            name: "Test Tag".to_string(),
+            slug: "test-tag".to_string(),
+            description: Some("A test tag description".to_string()),
+            date_created: None,
+        };
+
+        // Act
+        let result = insert(&pool, new_tag).await;
+
+        // Assert
+        assert!(result.is_ok());
+        let inserted_tag = result.unwrap();
+        assert_eq!(inserted_tag.name, "Test Tag");
+        assert_eq!(inserted_tag.slug, "test-tag");
+        assert_eq!(inserted_tag.description.unwrap(), "A test tag description");
+    }
+}
